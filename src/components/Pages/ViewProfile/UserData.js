288a234df79126ae052f486d1d73bd52a805aff6
Abdur-Rahman-Argon <106532958+Abdur-Rahman-Argon@users.noreply.github.com>
2022-09-16 23:55:51 +0600

    View User Profile

---
+++ src/components/Pages/ViewProfile/UserData.js
@@ -0,0 +1,211 @@
+import React from "react";
+
+import pic from "./../../../Images/149071.png";
+
+const UserData = ({ userInfo }) => {
+  const {
+    photoURL,
+    displayName,
+    profileHeading,
+    profileBio,
+    DateOfBirth,
+    Gander,
+    relationShip,
+  } = userInfo?.user;
+
+  console.log(userInfo);
+  return (
+    <div>
+      <div className="mt-1 shadow-sm bg-slate-100 py-2 px-5 font-semibold border-[1px] ">
+        <h1 className=" text-lg flex items-center gap-5">
+          <span>
+            <i class="fa-solid fa-eye"></i>
+          </span>
+          <span>View Profile</span>
+        </h1>
+      </div>
+
+      <div>{/* banner Image */}</div>
+
+      {/* Personal Info */}
+      <div className=" relative border-[1px] px-3 py-3 grid grid-cols-1 md:grid-cols-2 gap-2 items-center">
+        {/* profile img */}
+        <div className=" w-32 mx-auto ">
+          <a href="/my-profile">
+            <img
+              src={photoURL || pic}
+              alt=""
+              className="rounded-full border-[1px] "
+            />
+          </a>
+        </div>
+
+        {/* my info */}
+        <div className=" ">
+          <h1 className=" text-xl font-bold md:text-left text-center">
+            {displayName}
+          </h1>
+          <h1 className=" text-[16px] font-semibold md:text-left text-center">
+            {profileHeading || "Profile Heading None"}
+          </h1>
+          <div>
+            <p
+              className=" my-2 text-sm font-[400] md:text-left text-center
+              "
+            ></p>
+            <p
+              className=" my-2 text-sm font-[400] md:text-left text-center
+              "
+            >
+              {profileBio || "No any Bio"}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Personal Info  */}
+      <div className=" border-[1px] my-4 rounded">
+        <div className=" bg-slate-100 flex justify-between items-center px-5 border-b-[1px]">
+          <h1 className=" text-lg font-semibold">Personal Information:</h1>
+        </div>
+
+        <div className=" px-4  py-2">
+          <div className="  text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div>
+              <span className=" mx-3">
+                <i class="fa-solid fa-calendar-days"></i>
+              </span>
+              <span>Date Of Birth:</span>
+            </div>
+            <div>{DateOfBirth}</div>
+          </div>
+
+          <div className=" text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div>
+              <span className="mx-2">
+                <i class="fa-solid fa-venus-mars"></i>
+              </span>
+              Gander:
+            </div>
+            <div>{Gander}</div>
+          </div>
+
+          <div className=" text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div>
+              <span className="mx-2">
+                <i class="fa-solid fa-venus-mars"></i>
+              </span>
+              Gander:
+            </div>
+            <div>{Gander}</div>
+          </div>
+
+          <div className="  text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div>
+              <span className="mx-2">
+                <i class="fa-solid fa-children"></i>
+              </span>
+              RelationShip:
+            </div>
+            <div>{relationShip || "None"}</div>
+          </div>
+        </div>
+      </div>
+
+      {/* Contact nfo  */}
+      <div className=" border-[1px] my-2">
+        <div className="bg-slate-100 flex justify-between items-center px-5 border-b-[1px]">
+          <h1 className=" text-lg font-semibold">Contact Information:</h1>
+          <div className="  "></div>
+        </div>
+        <div className=" px-4  py-2">
+          <div className="  text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div>
+              <span className=" mx-[6px]">
+                <i class="fa-solid fa-location-dot"></i>
+              </span>
+              <span>Address:</span>
+            </div>
+            <div>Ponut, Kalai, JoypurHat</div>
+          </div>
+          <div className=" text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div>
+              <span className="mx-1">
+                <i class="fa-solid fa-phone"></i>
+              </span>
+              Phone:
+            </div>
+            <div>+880175464</div>
+          </div>
+          <div className="  text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div>
+              <span className="mx-1">
+                <i class="fa-solid fa-envelope"></i>
+              </span>{" "}
+              Email:
+            </div>
+            <div>abdur@gmail.com</div>
+          </div>
+        </div>
+      </div>
+
+      {/* Social Links  */}
+      <div className=" border-[1px] my-2">
+        <div className="bg-slate-100 flex justify-between items-center px-5 border-b-[1px]">
+          <h1 className=" text-lg font-semibold">Social Links</h1>
+          <div className="  "></div>
+        </div>
+        <div className=" px-4 py-2 ">
+          <div className=" text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div> Address: </div>
+            <div>Ponut, Kalai, JoypurHat</div>
+          </div>
+        </div>
+      </div>
+
+      {/* Education Info  */}
+      <div className=" border-[1px] my-2">
+        <div className="bg-slate-100 flex justify-between items-center px-5 border-b-[1px]">
+          <h1 className=" text-lg font-semibold">Education Information</h1>
+          <div className="  "></div>
+        </div>
+        <div className=" px-4 py-2 ">
+          <div className=" text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div> Address: </div>
+            <div>Ponut, Kalai, JoypurHat</div>
+          </div>
+        </div>
+      </div>
+
+      {/*  */}
+      <div className=" border-[1px] my-2">
+        <div className="bg-slate-100 flex justify-between items-center px-5 border-b-[1px]">
+          <h1 className=" text-lg font-semibold">Social Links</h1>
+          <div className="  "></div>
+        </div>
+        <div className=" px-4 py-2 ">
+          <div className=" text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div> Address: </div>
+            <div>Ponut, Kalai, JoypurHat</div>
+          </div>
+        </div>
+      </div>
+
+      {/*  */}
+      <div className=" border-[1px] my-2">
+        <div className="bg-slate-100 flex justify-between items-center px-5 border-b-[1px]">
+          <h1 className=" text-lg font-semibold">Social Links</h1>
+          <div className="  "></div>
+        </div>
+        <div className=" px-4 py-2 ">
+          <div className=" text-[15px]  my-[2px] font-semibold flex justify-between items-center ">
+            <div> Address: </div>
+            <div>Ponut, Kalai, JoypurHat</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserData;
---
+++ src/components/Pages/ViewProfile/ViewProfile.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import auth from "../../../firebase.init";
+import ProfileBlogs from "../Profile/ProfileBlogs";
+import UserData from "./UserData";
+
+const ViewProfile = () => {
+  const [user, loading] = useAuthState(auth);
+
+  const { userId } = useParams();
+
+  const userEmail = userId;
+
+  const {
+    data: userInfo,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery("myInfo", () =>
+    fetch(
+      `https://ancient-eyrie-83116.herokuapp.com/viewUser/${userEmail}`
+    ).then((res) => res.json())
+  );
+
+  if (isLoading || loading) {
+    return;
+  }
+  return (
+    <div className=" flex flex-col lg:flex-row justify-between items-start">
+      <div className=" flex-1 w-full px-5 lg:px-0">
+        {userInfo && <UserData userInfo={userInfo}></UserData>}
+      </div>
+      <div className=" mt-10 lg:mt-1 lg:w-5/12">
+        <ProfileBlogs></ProfileBlogs>
+      </div>
+    </div>
+  );
+};
+
+export default ViewProfile;
